eff8ee15ceea316f6ad6a88831a5e09926c69416
The-Bhavya <bhavyasquadleader@gmail.com>
2025-09-03 15:07:59 +0530

    ....

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
