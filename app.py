85c956985b203dbec8561574f924b803e588c047
The-Bhavya <bhavyasquadleader@gmail.com>
2025-09-06 10:29:41 +0530

    ...

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for,session, flash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
 import pandas as pd
 import joblib
 from pathlib import Path
 from datetime import datetime
+
 
 app = Flask(__name__)
 
